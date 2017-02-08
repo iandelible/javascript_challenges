8acd58799c1a95fb6c5d7996b290f3e875ea2875
Ian Pan <ianpan@hotmail.com>
2017-02-08 06:37:02 +0000

    practice with strict mode

---
+++ card.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Card {
+  constructor(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+    
+  }
+}
--- foobar.js
+++ foobar.js
@@ -3,9 +3,7 @@ var readlineSync = require('readline-sync');
 var input = readlineSync.question("How many digits of FooBar shall I produce: ");
 
 for (var iteration = 1; iteration <= Number(input); iteration++) {
-  if (iteration % 3 === 0 && iteration % 5 === 0) {
-    console.log("FooBar");
-  }
+  if (iteration % 3 === 0)
   else if (iteration % 3 === 0) {
     console.log("Foo");
   }
