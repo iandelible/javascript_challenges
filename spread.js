c5dd91b5f596748f7f0961f84cc0a6c9b78f4d77
Ian Pan <ianpan@hotmail.com>
2017-02-15 21:43:52 +0000

    spread.js practice

--- spread.js
+++ spread.js
@@ -1,10 +1,7 @@
 "use strict";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const newNumbers = [
-  // include all the items inside the numbers array
-  // without the enclosing array.
-  ...numbers,
+const newNumbers = [...numbers,
   10
 ];
 
